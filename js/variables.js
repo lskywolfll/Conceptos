d74f567bc1cb32596e685974091eb9dc8cfb60b3
Rene <renealejandrosanchezmorales@gmail.com>
2021-02-05 18:03:41 -0300

    se termina el ejemplo como tal para el tema de las variables

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+
+
+var nombre = "Rene";
+let nombre2 = "Rene";
+const apellidos = "Sanchez Morales";
+
+console.log(nombre)
+console.log(nombre2)
+console.log(apellidos)
+
+// El var y let, se les puede cambiar su valor por un nuevo 
+nombre = "Juan";
+nombre2 = "Pepito";
+
+console.log(nombre)
+console.log(nombre2)
+
+// Las constantes no se le puede dar otro valor, ya que siempre tendran el valor indicado al inicio y jamas cambiaran es eterno...
+apellidos = "";
+
+console.log(apellidos)
